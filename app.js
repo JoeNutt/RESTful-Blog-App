5161b7ce9086736455c1340873a5dd88f7dbbdc6
JoeNutt <joenutt01@gmail.com>
2020-07-31 22:12:10 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+//Monggose
+
+mongoose
+	.connect('mongodb://localhost:27017/restful_blog_app', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log('Connected to DB!'))
+	.catch((error) => console.log(error.message));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+// Blog Schema Config
+const blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: { type: Date, default: Date.now }
+});
+//Model compile
+const Blog = mongoose.model('Blog', blogSchema);
+// INDEX ROUTE
+app.get('/', function(req, res) {
+	res.redirect('/blogs');
+});
+app.get('/blogs', function(req, res) {
+	Blog.find({}, function(err, blogs) {
+		if (err) {
+			console.log('err');
+		} else {
+			res.render('index', { blogs: blogs });
+		}
+	});
+});
+// NEW ROUTE
+app.get('/blogs/new', (req, res) => {
+	res.render('new');
+});
+
+// CREATE ROUTE
+
+app.post('/blogs', function(req, res) {
+	Blog.create(req.body.blog, function(err, newBlog) {
+		if (err) {
+			res.render('new');
+		} else {
+			res.render('/blogs');
+		}
+	});
+});
+// PORT
+app.listen(3000, () => console.log('listening on http://localhost:3000/')); // new
