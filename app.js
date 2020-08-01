4cd77939749f2278c55b45f55dd64f1af2b02d98
JoeNutt <joenutt01@gmail.com>
2020-08-01 13:00:30 +0100

    Added Blog publishing

--- app.js
+++ app.js
@@ -27,30 +27,35 @@ const Blog = mongoose.model('Blog', blogSchema);
 app.get('/', function(req, res) {
 	res.redirect('/blogs');
 });
+
 app.get('/blogs', function(req, res) {
 	Blog.find({}, function(err, blogs) {
 		if (err) {
-			console.log('err');
+			console.log('ERROR');
 		} else {
 			res.render('index', { blogs: blogs });
 		}
 	});
 });
+// END INDEX
+
 // NEW ROUTE
 app.get('/blogs/new', (req, res) => {
 	res.render('new');
 });
+// END NEW
 
-// CREATE ROUTE
-
-app.post('/blogs', function(req, res) {
+// CREATE
+app.post('/blogs', (req, res) => {
 	Blog.create(req.body.blog, function(err, newBlog) {
 		if (err) {
 			res.render('new');
 		} else {
-			res.render('/blogs');
+			res.redirect('/blogs');
 		}
 	});
 });
+// END CREATE
 // PORT
-app.listen(3000, () => console.log('listening on http://localhost:3000/')); // new
+
+app.listen(3000, () => console.log('listening on http://localhost:3000/')); //
